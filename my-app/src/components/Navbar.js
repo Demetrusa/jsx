b6007c828a17e1d0c47acbe8b7628504e62dfda8
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-02-23 23:44:39 +0400

    created button component added CSs

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,10 +1,25 @@
 import React, {useState} from 'react'
+import { Button } from './Button';
+import './Navbar.css';
+
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu  = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <=960) {
+            setButton(false);
+        }else{
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton)
+
   return (
    <>
    <nav className="navbar">
@@ -29,6 +44,7 @@ function Navbar() {
             <a className='nav-links-mobile' href="#" onClick={closeMobileMenu}>Sign Up</a>
             </li>
         </ul>
+        {button && <Button buttonStyle="btn--outline">Sign UP</Button>}
        </div>
    </nav>
    </>
